527e14d0d149a44a2ad20593bef8b5324173e423
HeatherNev88 <heather.alv88@yahoo.com>
2020-12-17 21:24:13 -0800

    home page styling finished with images

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 // import Container from './components/Container';
+import Header from './components/Header'
+import Map from './components/Map'
 
 import Home from './components/Home';
 
@@ -11,7 +13,8 @@ function App() {
   if (isLoading) return <div>Loading...</div>
 
   return (
-    
+
+      <Header />,
       <Home />
       
     );
